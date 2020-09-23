54ee26d5b50b57a658aef3650a13e08ddba9047f
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-09-22 21:39:50 -0400

    Done.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,6 @@
-'has a summary' do
-    post = Post.create!(title: "My title", description: "The post description")
-    expect(post.post_summary).to eq("My title - The post description")
-  
+describe Post do
+    it 'can be created' do
+      post = Post.create!(title: "My title", description: "The post description")
+      expect(post).to be_valid
+    end
+  end
